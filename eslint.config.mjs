499ab04826beb73cddf034121ee1e6d0ae6338fc
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2025-05-23 10:52:01 +0200

    Migrate to new eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,68 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next',
+    'eslint:recommended',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ),
+  {
+    languageOptions: {
+      parser: require.resolve('@typescript-eslint/parser'),
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    plugins: {
+      prettier: require('eslint-plugin-prettier'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+    },
+    rules: {
+      // JavaScript rules
+      'prefer-const': 'warn',
+      'no-var': 'warn',
+      'no-unused-vars': 'warn',
+      'object-shorthand': 'warn',
+      'quote-props': ['warn', 'as-needed'],
+      // TypeScript rules
+      '@typescript-eslint/array-type': ['warn', { default: 'array' }],
+      '@typescript-eslint/consistent-type-assertions': [
+        'warn',
+        {
+          assertionStyle: 'as',
+          objectLiteralTypeAssertions: 'never',
+        },
+      ],
+      // React rules
+      'react/jsx-fragments': ['warn', 'syntax'],
+      'react/jsx-filename-extension': ['warn', { extensions: ['ts', 'tsx'] }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
